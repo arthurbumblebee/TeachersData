ca06dcb09771302fd420068002b5541ee7e195fe
Arthur <amakumbi@colby.edu>
2018-06-14 17:15:33 -0400

    fixing date range irregularities

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,20 +1,47 @@
-$( function() {
-    $( "#datepicker" ).click(function(){
-        $( "#datepicker" ).hide();
+$(function () {
+    $("#datepicker").click(function () {
+        $("#datepicker").hide();
         // datepicker();
-    }) 
-} );
+    })
+});
 
-$( function pickDate(){
-    var date_input=$('input[name="start_date"]');
-    var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
-    var options={
+$(function pickDate() {
+    var date_input = $('input[name="date"]');
+    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
+    var options = {
         format: 'mm/dd/yyyy',
         container: container,
         todayHighlight: true,
         autoclose: true,
     };
-    date_input.datepicker(options);
-    $("#start_date").datepicker(options);
-    $("#end_date").datepicker(options);
+    // date_input.datepicker(options);
+
+    $('#start_date').datetimepicker();
+    $('#end_date').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+
+    });
+    $("#start_date").on("dp.change", function (e) {
+        $('#end_date').data("DateTimePicker").minDate(e.date);
+        $('#end_date').data("DateTimePicker").minDate
+    });
+    $("#end_date").on("dp.change", function (e) {
+        $('#start_date').data("DateTimePicker").maxDate(e.date);
+    });
+
+    // $("#start_date").datepicker(options);
+    // $("#end_date").datepicker(options);
 })
+
+$(function () {
+    $('#datetimepicker6').datetimepicker();
+    $('#datetimepicker7').datetimepicker({
+        useCurrent: false //Important! See issue #1075
+    });
+    $("#datetimepicker6").on("dp.change", function (e) {
+        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
+    });
+    $("#datetimepicker7").on("dp.change", function (e) {
+        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
+    });
+});
