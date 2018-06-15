63c09a077656b62779b8a474548b03fd683204e1
Arthur <amakumbi@colby.edu>
2018-06-15 12:46:58 -0400

    fixed choosing dates, and reorganized js and bootstrap imports

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,47 +1,20 @@
-$(function () {
-    $("#datepicker").click(function () {
-        $("#datepicker").hide();
-        // datepicker();
-    })
-});
-
-$(function pickDate() {
-    var date_input = $('input[name="date"]');
-    var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
-    var options = {
-        format: 'mm/dd/yyyy',
-        container: container,
-        todayHighlight: true,
-        autoclose: true,
-    };
-    // date_input.datepicker(options);
-
-    $('#start_date').datetimepicker();
-    $('#end_date').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-
+// choose dates
+$(function chooseDate() {
+    // http://momentjs.com/docs/#/displaying/format/ - to see formats
+    $('#start_date').datetimepicker({
+        format: 'L',
+        ignoreReadonly: true
     });
+    $('#end_date').datetimepicker({
+        useCurrent: false,
+        format: 'L',
+        ignoreReadonly: true
+    });
+    // disable choosing invalid dates
     $("#start_date").on("dp.change", function (e) {
         $('#end_date').data("DateTimePicker").minDate(e.date);
-        $('#end_date').data("DateTimePicker").minDate
     });
     $("#end_date").on("dp.change", function (e) {
         $('#start_date').data("DateTimePicker").maxDate(e.date);
     });
-
-    // $("#start_date").datepicker(options);
-    // $("#end_date").datepicker(options);
 })
-
-$(function () {
-    $('#datetimepicker6').datetimepicker();
-    $('#datetimepicker7').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-    });
-    $("#datetimepicker6").on("dp.change", function (e) {
-        $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-    });
-    $("#datetimepicker7").on("dp.change", function (e) {
-        $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-    });
-});
