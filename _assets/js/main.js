f6f7c711270618a478ff0621e112ceff984e71dd
Arthur <amakumbi@colby.edu>
2018-07-05 13:32:46 -0400

    cleaning up

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -285,15 +285,3 @@ $(function downloadFile() {
         window.location.href = "data/recordedData/" + $("#selectLocation").val() + ".csv";
     })
 });
-
-/** UPLOAD */
-$(function() {
-    $('#fileupload').fileupload({
-        dataType: 'json',
-        done: function(e, data) {
-            $.each(data.result.files, function(index, file) {
-                $('<p/>').text(file.name).appendTo(document.body);
-            });
-        }
-    });
-});
