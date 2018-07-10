8dc68220cbfeec14efdb972514505de76e15e313
Arthur <amakumbi@colby.edu>
2018-07-10 12:59:39 -0400

    modifying script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Deploy script for Harp website
+# Deploy script for Jekyll website
 
 jekyll build && rsync -az --delete _site www-edit@ocean-dev:/var/www/html/envdata.bigelow.org/
 
